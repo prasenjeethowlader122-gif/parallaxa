6de2895d91c3633374b6faaa9930ba8e820b30a3
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-03-13 00:48:11 +0600

    update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image';
-import profilePic from '../public/logo-text.png'
+import profilePic from '../public/New Project 20 [79DB18E].png'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
