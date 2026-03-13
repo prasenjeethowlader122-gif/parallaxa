5ee54442eb663ec6cdeddcce675fb71461668bc6
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-03-13 15:07:28 +0600

    update

---
+++ lib/firebase/index.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCN_8OjsJMDBBDg0hckB5IU5yRSVUZbhyY",
+  authDomain: "chirpstream-dg7rv.firebaseapp.com",
+  projectId: "chirpstream-dg7rv",
+  storageBucket: "chirpstream-dg7rv.firebasestorage.app",
+  messagingSenderId: "534372451622",
+  appId: "1:534372451622:web:e70828f474faad88512de7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
