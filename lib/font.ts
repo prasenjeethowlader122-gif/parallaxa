7692bfcd2cc0850b2d059579cc088f11abf3b91e
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-03-14 23:54:17 +0600

    update font.ts

--- lib/font.ts
+++ lib/font.ts
@@ -2,17 +2,25 @@
 import localFont from 'next/font/local'
 
 export const spacegrotesk = localFont({
-  src: '../public/local/font/SpaceGrotesk-VariableFont_wght.ttf',
+  src: '../public/local/font/AtkinsonHyperlegibleNext-VariableFont_wght.ttf',
   variable: '--font-space-grotesk',
 })
 
 export const slabo = localFont({
   src: [
     {
-      path: '../public/local/font/Slabo27px-Regular.ttf',
+      path: '../public/local/font/AbhayaLibre-Regular.ttf',
       weight: '400',
       style: 'normal',
     },
+    {
+      
+      path: '../public/local/font/AbhayaLibre-Bold.ttf',
+      weight: '600',
+      style: 'normal',
+    
+    }
+    
   ],
   variable: '--font-slabo',
 })
