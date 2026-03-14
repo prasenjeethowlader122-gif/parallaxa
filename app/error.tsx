d10f08dd2d356f9b77b038a63128c2a059d4751e
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-03-14 15:03:59 +0600

    update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -12,7 +12,7 @@ export default function GlobalError({
     <html>
       <body>
         <h2>Something went wrong!</h2>
-        <small>{error.message}</small>
+        <small>{error.message + '<br/>' + error.digest}</small>
         <button onClick={() => reset()}>Try again</button>
       </body>
     </html>
