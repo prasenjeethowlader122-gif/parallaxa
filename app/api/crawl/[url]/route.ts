2914bd38b62f146acdeece4630e4acf66954e85d
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-03-14 20:49:48 +0600

    update route.ts

--- app/api/crawl/[url]/route.ts
+++ app/api/crawl/[url]/route.ts
@@ -9,7 +9,7 @@ export async function GET(
     
     const firecrawl = new Firecrawl({ apiKey: "fc-da0837003c26469da0f8c259c6c10944" });
     const { url } = await params
-    const res = await firecrawl.map('https://news.google.com', { limit: 50 });
+    const res = await firecrawl.map('https://news.google.com/stories/CAAqNggKIjBDQklTSGpvSmMzUnZjbmt0TXpZd1NoRUtEd2lqbk5ETEVCRTY3djFqNFJBWVhTZ0FQAQ?ceid=US:en&oc=3', { limit: 50 });
     
     return NextResponse.json(res)
   } catch (e) {
