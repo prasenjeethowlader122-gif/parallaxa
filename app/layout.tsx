14c04cfa6dbbb027d29d31047d9eb1868fcbf8da
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-03-14 15:20:41 +0600

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   children: React.ReactNode
 } > ) {
   return (
-    <html lang="en" className={`${spacegrotesk.className} ${slabo.className}`}>
+    <html lang="en" className={`${spacegrotesk.className}`}>
      
       <body className="antialiased bg-white text-gray-900">
         <ClientSessionProvider>
