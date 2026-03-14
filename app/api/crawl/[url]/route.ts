30792027bdcacbc7c880f05965ef41299ad7501c
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-03-14 20:34:06 +0600

    update

--- app/api/crawl/[url]/route.ts
+++ app/api/crawl/[url]/route.ts
@@ -1,27 +1,21 @@
-import { CheerioCrawler, EnqueueStrategy } from 'crawlee';
+
 import { NextRequest, NextResponse } from 'next/server'
+import Firecrawl from '@mendable/firecrawl-js';
 
 
 export async function GET(
-  _: NextRequest, { params }: { params: Promise < { id: string } > }
+  _: NextRequest, { params }: { params: Promise < { url: string } > }
 ) {
   try {
-    const crawler = new CheerioCrawler({
-      maxRequestsPerCrawl: 10, // Limitation for only 10 requests (do not use if you want to crawl all links)
-      async requestHandler({ request, enqueueLinks, log }) {
-        log.info(request.url);
-        await enqueueLinks({
-          // Setting the strategy to 'all' will enqueue all links found
-          strategy: EnqueueStrategy.All,
-          // Alternatively, you can pass in the string 'all'
-          // strategy: 'all',
-        });
-      },
-    });
     
-    // Run the crawler with initial request
-    const r =  await crawler.run(['https://crawlee.dev']);
-    return NextResponse.json(r)
+    const firecrawl = new Firecrawl({ apiKey: "fc-da0837003c26469da0f8c259c6c10944" });
+    const { url } = await params
+    const results = await firecrawl.search(url, {
+      limit: 3,
+      scrapeOptions: { formats: ['markdown'] }
+    });
+    console.log(results);
+    return NextResponse.json(results)
   } catch (e) {
     return NextResponse.json({})
   }
