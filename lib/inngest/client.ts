b0a44a59282ea8064106d8917b1b8219e4e1a5ef
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-03-17 00:33:45 +0600

    update client.ts

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -1,3 +1,3 @@
 import { Inngest } from 'inngest'
 
-export const inngest = new Inngest({ id: 'yahoo-news-pipeline' })
+export const inngest = new Inngest({ id: 'yahoo-news-pipeline' , eventKey: 'yOGp_Ld6htY1J5eQMEUa_WwqWpB1jcsD7D5QqQaEIHR8axFt0tPbNqhhqbsraDgkBumrCeOkH0xSBnoYfXApSw'})
