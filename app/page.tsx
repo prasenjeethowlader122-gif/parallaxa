abb6ad585d67ecd676d11e6b5a422849c48926b8
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-03-23 17:26:25 +0600

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -287,7 +287,7 @@ export default function Home() {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
               <div className="lg:col-span-2">
-                <div className="flex flex-row items-center justify-between mb-4">
+                <div className="flex flex-row items-center justify-between mb-4 pb-4 border-2 border-gray-800">
                   <h2 className="text-2xl font-bold text-gray-900">World</h2>
                   <span className="md:hidden text-sm text-blue-600 cursor-pointer">See all</span>
                 </div>
