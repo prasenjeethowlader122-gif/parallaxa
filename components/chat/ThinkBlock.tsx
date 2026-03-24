8a0e01f2e03522cdb7012b54ed39a5759877a351
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-03-24 16:56:17 +0600

    Add improved thinking visualization component

---
+++ components/chat/ThinkBlock.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ThinkBlock = () => {
+    return (
+        <div className="think-block">
+            <h2>Thinking Visualization</h2>
+            <p>This component visualizes thinking processes and brainstorming.</p>
+            <div className="visualization"> {/* Placeholder for visual elements */} </div>
+        </div>
+    );
+};
+
+export default ThinkBlock;
