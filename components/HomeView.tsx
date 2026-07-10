1c8ab9eda545b15ac23b9995ad2c331073259904
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-07-10 23:57:14 +0600

    update HomeView.tsx

--- components/HomeView.tsx
+++ components/HomeView.tsx
@@ -136,8 +136,7 @@ function CoverFlowSlider({ articles }: { articles: NewsArticle[] }) {
         >
           <span className="block w-2 h-2 border-r border-b border-gray-500 -rotate-45 -translate-x-px" />
         </button>
-      </div> <
-    /div>
+      </div> </div>
   )
 }
 
