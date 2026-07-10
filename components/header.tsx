c44f107285fa23a5c78949ab4f01f54be42e7447
prasenjeethowlader122-gif <prasenjeethowlader122@gmail.com>
2026-07-10 22:44:54 +0600

    update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -364,7 +364,7 @@ export function Header({
                   priority
                   className="h-14 w-auto"
                 />**/}
-                <h1 className = {`${banglaFontlogo.className} text-2xl font-[900]`}>অনলি্হিন্দু™</h1>
+                <h1 className = {`${banglaFont.className} text-2xl font-[900]`}>অনলি্হিন্দু™</h1>
                   
               </div>
             </Link>
